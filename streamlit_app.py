6c02687f03145b638a75cb1242e241e389a3f4e9
Brendan Hasz <winsto99@gmail.com>
2022-12-06 13:52:51 -0600

    add significance stars

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,18 @@ gene_data_female = df_female[df_female["Gene Symbol"] == gene_select].iloc[0, :]
 # Get columns to plot
 col1, col2 = comparison_select.split(" vs ")
 
+# P-values
+p_value_threshold = 0.05
+pvalue_map = {
+    "WT Fracture vs WT Control": "Adj-p WT Fx vs WT Ctrl",
+    "DTR Fracture vs WT Fracture": "Adj-p DTR Fx vs WT FX",
+    "DTR Fracture vs DTR Control": "Adj-p DTR Fx vs DTR Ctrl",
+    "DTR Control vs WT Control": "Adj-p DTR Ctrl vs WT Ctrl",
+}
+male_p_value = gene_data_male[pvalue_map[comparison_select]]
+female_p_value = gene_data_female[pvalue_map[comparison_select]]
+
+
 # Bar chart
 fig = go.Figure()
 fig.add_trace(
@@ -72,18 +84,46 @@ fig.add_trace(
         error_y=dict(type='data', array=[gene_data_male["SEM "+col2], gene_data_female["SEM "+col2]])
     )
 )
-fig.add_trace(
-    go.Scatter(
-        x=[0, 1.0, 2.0],
-        y=[0.0, 1000.0, 2000.0],
-        mode="markers",  # or "lines"
-    )
+star_y = 1.05 * (
+    np.max([gene_data_male[col1], gene_data_female[col1], gene_data_male[col2], gene_data_female[col2]])
+    + np.max([gene_data_male["SEM "+col1], gene_data_female["SEM "+col1], gene_data_male["SEM "+col2], gene_data_female["SEM "+col2]])
 )
+if male_p_value < p_value_threshold:
+    fig.add_trace(
+        go.Scatter(
+            name=f"M p={male_p_value}",
+            x=["Male"],
+            y=[star_y],
+            mode="markers",  # or "lines"
+            marker_symbol="asterisk",  # or "star"
+            marker_line_color="black",
+            marker_color="black",
+            marker_line_width=1,
+            marker_size=10,
+        )
+    )
+if female_p_value < p_value_threshold:
+    fig.add_trace(
+        go.Scatter(
+            name=f"F p={male_p_value}",
+            x=["Feale"],
+            y=[star_y],
+            mode="markers",  # or "lines"
+            marker_symbol="asterisk",  # or "star"
+            marker_line_color="black",
+            marker_color="black",
+            marker_line_width=1,
+            marker_size=10,
+        )
+    )
 fig.update_layout(
     barmode='group',
     yaxis_title="TODO: y axis label",
     font=dict(size=12),
 )
+for trace in fig['data']: 
+    if(trace['name'] not in [col1, col2]):
+        trace['showlegend'] = False
 st.plotly_chart(fig, use_container_width=True)
 
 col1, col2 = st.columns(2)
@@ -101,13 +141,7 @@ with col1:
     st.write(f"Female: *{gene_data_female[fold_change_map[comparison_select]]}*")
 
 # P-values
-pvalue_map = {
-    "WT Fracture vs WT Control": "Adj-p WT Fx vs WT Ctrl",
-    "DTR Fracture vs WT Fracture": "Adj-p DTR Fx vs WT FX",
-    "DTR Fracture vs DTR Control": "Adj-p DTR Fx vs DTR Ctrl",
-    "DTR Control vs WT Control": "Adj-p DTR Ctrl vs WT Ctrl",
-}
 with col2:
     st.write("P values:")
-    st.write(f"Male: p=*{gene_data_male[pvalue_map[comparison_select]]}*")
-    st.write(f"Female: p=*{gene_data_female[pvalue_map[comparison_select]]}*")
+    st.write(f"Male: p=*{male_p_value}*")
+    st.write(f"Female: p=*{female_p_value}*")
