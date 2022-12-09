9d2d13fd0f02383c781581899d1f232f4810a69c
Brendan Hasz <winsto99@gmail.com>
2022-12-08 16:59:01 -0800

    md

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,11 +70,11 @@ st.write(pvalues["Transcript ID"].dtype)  # TODO debugging
 st.write(all_gene_data_male["Transcript ID"].dtype)  # TODO debugging
 st.write(type(pvalues))  # TODO debugging
 st.write(type(all_gene_data_male))  # TODO debugging
-pvalues = pvalues.merge(all_gene_data_male[["Transcript ID", pvalue_col]], on="Transcript ID", suffixes=('_x', '_m'))
+pvalues = pvalues.merge(all_gene_data_male[["Transcript ID", pvalue_col]], on="Transcript ID")
 st.write(pvalues)  # TODO debugging
-pvalues = pvalues.merge(all_gene_data_female[["Transcript ID", pvalue_col]], on="Transcript ID", suffixes=('_x', '_f'))
+pvalues = pvalues.merge(all_gene_data_female[["Transcript ID", pvalue_col]], on="Transcript ID")
 st.write(pvalues)  # TODO debugging
-pvalues["avg pvalue"] = (pvalues[pvalue_col+"_m"] + pvalues[pvalue_col+"_m"]) / 2
+pvalues["avg pvalue"] = (pvalues[pvalue_col] + pvalues[pvalue_col+"_y"]) / 2
 pvalues.sort_values("avg pvalue", inplace=True)
 st.write(pvalues)
 sorted_transcript_ids = pvalues["Transcript ID"].tolist()
